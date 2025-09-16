0571cf129bdbaa4faa8f8b782e0abad59c358e38
IT24101699 <it24101699@my.sliit.lk>
2025-09-16 22:59:54 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24101699_PS_Lab07/IT24101699_PS_Lab07.R
@@ -0,0 +1,12 @@
+getwd()
+setwd("//Users//shehanitharuka//Documents//IT24101699_PS_Lab07")
+
+#Q1
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#Q2
+pexp(2, rate = 13) 
+
+#Q3
+1 - pnorm(130, mean = 100, sd = 15) 
+qnorm(0.95, mean = 100, sd = 15)   
